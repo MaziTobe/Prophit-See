6e13ba6e55d719de7abe2d36df60612765c939a1
Mazi Tobe <emma.too50@googlemail.com>
2025-12-05 23:11:22 +0100

    updated email in contact.js to reflect characters real email account

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,7 +5,7 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
 
-    const email = "onwuraemmanuel90@googlemail.com";
+    const email = "seeprophit@gmail.com";
 
     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
 
